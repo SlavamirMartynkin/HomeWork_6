0dfae01324078f8f3e039a33fefee1f8a335f848
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-04-27 14:47:44 +0300

    Task_43.1

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -20,6 +20,24 @@ int ReadInt(string argument) // проверка на число
     return i;
 }
 
+bool Validate(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2)
+        {
+            System.Console.WriteLine("прямые совпадают");
+            return false;
+        }
+        else
+        {
+            System.Console.WriteLine("прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
+
 double[] KrossPoint(double b1, double k1, double b2, double k2)
 {
     double x = (b2 - b1) / (k1 - k2);
@@ -29,6 +47,7 @@ double[] KrossPoint(double b1, double k1, double b2, double k2)
     array[0] = x;
     array[1] = y;
     return array;
+    
 }
 
 int b1 = ReadInt("Введите b1: ");
@@ -36,4 +55,7 @@ int k1 = ReadInt("Введите k1: ");
 int b2 = ReadInt("Введите b2: ");
 int k2 = ReadInt("Введите k2: ");
 
-System.Console.WriteLine($"[{string.Join(",", KrossPoint(b1, k1, b2, k2))}]");
+if (Validate(b1, k1, b2, k2))
+{
+    System.Console.WriteLine($"[{string.Join(",", KrossPoint(b1, k1, b2, k2))}]");
+}
